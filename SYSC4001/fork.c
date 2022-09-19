7f11a1381c92c37567fa59f7549a6d242f3599e3
lorienaquarius <71980093+lorienaquarius@users.noreply.github.com>
2022-09-19 14:55:47 -0400

    Grand Child
    added proper grand-child implementation

--- SYSC4001/fork.c
+++ SYSC4001/fork.c
@@ -23,20 +23,35 @@ int main(int argc, char *argv[]){
 			perror("fork failed");
 			exit(1);
 		case 0:
-			//fork();
-			message = "This is the child";
-			//printf("Value of child PID is: %d, PPID is: %d\n", getpid(), getppid());
-			n = 5;
 			gettimeofday(&end, NULL);
-			printf("Time to get to child block: %ld micro-seconds\n", ((end.tv_sec * MICRO_SEC_IN_SEC + end.tv_usec) - (start.tv_sec * MICRO_SEC_IN_SEC + start.tv_usec)));
+			printf("Time to get to child block: %ld micro-seconds\n", ((end.tv_sec 						* MICRO_SEC_IN_SEC + end.tv_usec) - (start.tv_sec * MICRO_SEC_IN_SEC + start.tv_usec)));
+			pid_t grand_pid=fork();
+			n=5;
 			
+			switch(grand_pid) {
+				case -1: //Error
+					perror("fork failed");
+					exit(1);
+				case 0:
+					gettimeofday(&end, NULL);
+			printf("Time to get to grand-child block: %ld micro-seconds\n", ((end.tv_sec 						* MICRO_SEC_IN_SEC + end.tv_usec) - (start.tv_sec * MICRO_SEC_IN_SEC + start.tv_usec)));
+					message="This is the grand-child\n";
+					n = 5;
+					break;
+				default:
+					message = "This is the child";
+					n = 3;
+					//printf("Value of parent PID is: %d\n", pid);
+					break;
+				}
+				
 			break;
 		default:
 			message = "This is the parent";
 			n = 3;
 			//printf("Value of parent PID is: %d\n", pid);
 			gettimeofday(&end, NULL);
-			printf("Time to get to parent block: %ld micro-seconds\n", ((end.tv_sec * MICRO_SEC_IN_SEC + end.tv_usec) - (start.tv_sec * MICRO_SEC_IN_SEC + start.tv_usec)));
+			printf("Time to get to parent block: %ld micro-seconds\n", ((end.tv_sec 						* MICRO_SEC_IN_SEC + end.tv_usec) - (start.tv_sec * MICRO_SEC_IN_SEC + start.tv_usec)));
 			break;
 	}
 	printf("My PID is %d, n = %d\n", getpid(), n);
