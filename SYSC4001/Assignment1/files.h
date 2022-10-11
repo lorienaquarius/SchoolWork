40b92bd0deba30e0fadb581bd837e8adb276d3c0
lorienaquarius <lorienaquarius@cmail.carleton.ca>
2022-10-11 11:44:37 -0400

    Updated header file and added compile file

--- SYSC4001/Assignment1/files.h
+++ SYSC4001/Assignment1/files.h
@@ -4,13 +4,15 @@
 #include <string.h>
 #include <fcntl.h>
 #include <limits.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #define CONTROLLER_FIFO "/tmp/controller_fifo"
+#define CLOUD_FIFO "/tmp/cloud_fifo"
 typedef struct sensor{
     pid_t sensor_pid;
     char name[256];
     int threshold;
     int value;
-    int is_actuator; //1 is actuator, 0 is sensor
+    int activated;
 } sensor;
