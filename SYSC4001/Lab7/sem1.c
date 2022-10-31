ca1a7f2265d1b4887711a5f2782aed15e45160df
lorienaquarius <lorienaquarius@cmail.carleton.ca>
2022-10-31 14:59:52 -0400

    Example works now

--- SYSC4001/Lab7/sem1.c
+++ SYSC4001/Lab7/sem1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/sem.h>
-
+#include "semun.h"
 
 
 static int set_semvalue(void);
@@ -10,6 +10,46 @@ static void del_semvalue(void);
 static int semaphore_p(void);
 static int semaphore_v(void);
 static int sem_id;
+
+static int set_semvalue(void)
+{
+    union semun sem_union;
+    sem_union.val = 1;
+    if (semctl(sem_id, 0, SETVAL, sem_union) == -1) return(0);
+    return(1);
+}
+
+static void del_semvalue(void)
+{
+    union semun sem_union;
+    if (semctl(sem_id, 0, IPC_RMID, sem_union) == -1)
+        fprintf(stderr, "Failed to delete semaphore\n");
+}
+
+static int semaphore_p(void)
+{
+    struct sembuf sem_b;
+    sem_b.sem_num = 0;
+    sem_b.sem_op = -1; /* P() */
+    sem_b.sem_flg = SEM_UNDO;
+    if (semop(sem_id, &sem_b, 1) == -1) {
+        fprintf(stderr, "semaphore_p failed\n");
+        return(0);
+    }
+    return(1);
+}
+static int semaphore_v(void)
+{
+    struct sembuf sem_b;
+    sem_b.sem_num = 0;
+    sem_b.sem_op = 1; /* V() */
+    sem_b.sem_flg = SEM_UNDO;
+    if (semop(sem_id, &sem_b, 1) == -1) {
+        fprintf(stderr, "semaphore_v failed\n");
+        return(0);
+    }
+    return(1);
+}
 
 
 int main(int argc, char *argv[])
@@ -46,42 +86,4 @@ int main(int argc, char *argv[])
     exit(EXIT_SUCCESS);
 }
 
-static int set_semvalue(void)
-{
-    union semun sem_union;
-    sem_union.val = 1;
-    if (semctl(sem_id, 0, SETVAL, sem_union) == -1) return(0);
-    return(1);
-}
 
-static void del_semvalue(void)
-{
-    union semun sem_union;
-    if (semctl(sem_id, 0, IPC_RMID, sem_union) == -1)
-        fprintf(stderr, “Failed to delete semaphore\n”);
-}
-
-static int semaphore_p(void)
-{
-    struct sembuf sem_b;
-    sem_b.sem_num = 0;
-    sem_b.sem_op = -1; /* P() */
-    sem_b.sem_flg = SEM_UNDO;
-    if (semop(sem_id, &sem_b, 1) == -1) {
-        fprintf(stderr, "semaphore_p failed\n");
-        return(0);
-    }
-    return(1);
-}
-static int semaphore_v(void)
-{
-    struct sembuf sem_b;
-    sem_b.sem_num = 0;
-    sem_b.sem_op = 1; /* V() */
-    sem_b.sem_flg = SEM_UNDO;
-    if (semop(sem_id, &sem_b, 1) == -1) {
-        fprintf(stderr, "semaphore_v failed\n");
-        return(0);
-    }
-    return(1);
-}
