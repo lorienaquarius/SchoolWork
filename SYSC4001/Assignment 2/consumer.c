7b5149aeb7bc5315c85b32527a49ed842fcd4c58
Lorien Aquarius <lorienaquarius@cmail.carleton.ca>
2022-11-13 02:15:21 -0500

    Seems to be working producer, need to work out semaphore N and proper signalling; printing null characters to file

---
+++ SYSC4001/Assignment 2/consumer.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#define NUM_BUFFER 100
+
+//Structure to hold 128 bytes from a text file and byte count
+typedef struct buffer{
+    char data[128];
+    int count;
+} buffer;
+
+
+int main(){
+
+    //Semaphore S
+    int S = semget((key_t) 1111, 1, 0777 | IPC_CREAT);
+
+    if(S == -1){
+        perror("Could not create semaphore S\n");
+    }
+
+    //Semaphore E
+    int E = semget((key_t) 2222, 1, 0777 | IPC_CREAT);
+    
+    if(E == -1){
+        perror("Could not create semaphore E\n");
+    }
+
+    //Semaphore N
+    int N = semget((key_t) 3333, 1, 0777 | IPC_CREAT);
+
+    if(N == -1){
+        perror("Could not create semaphore N\n");
+    }
+
+    //structs for semop()
+    struct sembuf wait = {0, -1, SEM_UNDO};
+    struct sembuf signal = {0, 1, SEM_UNDO};
+    
+    
+    int write_file_fd = open("write.txt", O_WRONLY);
+
+    if(write_file_fd == -1){
+        perror("Could not open read file\n");
+    }
+
+    int shm_id = shmget((key_t) 1234, (size_t) (sizeof(buffer) * NUM_BUFFER), 0777 | IPC_CREAT);
+    
+    if(shm_id == -1){
+        perror("could not create shared memory\n");
+    }
+    buffer *shm;
+    shm = (buffer *)shmat(shm_id, (void *)0, SHM_RND);
+    if(shm == (buffer *)-1){
+        perror("could not attach shared memory\n");
+    }
+
+    buffer temp;
+    int done = 1;
+    long int byte_count;
+    int success;
+    int out = 0;
+    while(done){
+        //wait(n), wait(s)
+        printf("Waiting on N\n");
+        success = semop(N, &wait, 1);
+        if(success == -1){
+            perror("Semaphore N operation wait failed\n");
+        }
+        printf("waiting on S\n");
+        success = semop(S, &wait, 1);
+        if(success == -1){
+            perror("Semaphore S operation wait failed\n");
+        }
+
+
+        strcpy(temp.data, shm[out].data);
+        temp.count = shm[out].count;
+        if(temp.data[0] == '\0'){
+            done = 0;
+        }
+        printf("Consumer copied over data as string: %s\n", shm[out].data);
+        out = (out + 1) % 100;
+        byte_count += sizeof(temp);
+
+
+        //signal(s), signal(e)
+        success = semop(S, &signal, 1);
+        
+        if(success == -1){
+            perror("Semaphore S operation signal failed\n");
+        }
+
+        success = semop(E, &signal, 1);
+
+        if(success == -1){
+            perror("Semaphore E operation signal failed\n");
+        }
+
+        success = write(write_file_fd, &temp.data, sizeof(temp.data));
+
+        if(success == -1){
+            perror("Write operation failed\n");
+        }
+
+    }
+    success = write(write_file_fd, '\0', 1);
+    if (success == -1){
+        printf("Failed to write null character to end of file\n");
+    }
+    printf("Total bytes read from shared memory: %ld\n", byte_count);
+    free(shm);
+    success = shmdt(shm);
+    if(success == -1){
+        printf("Could not detach shared memory\n");
+    }
+
+    success = semctl(S, 0, IPC_RMID, 0);
+
+    if(success == -1){
+        printf("Could not delete semaphore S\n");
+    }
+    success = semctl(E, 0, IPC_RMID, 0);
+
+    if(success == -1){
+        printf("Could not delete semaphore E\n");
+    }
+
+    success = semctl(N, 0, IPC_RMID, 0);
+
+    if(success == -1){
+        printf("Could not delete semaphore N\n");
+    }
+
+    success = close(write_file_fd);
+
+    if (success == -1){
+        printf("Could not close write file\n");
+    }
+    
+
+}
--- SYSC4001/Assignment 2/producer.c
+++ SYSC4001/Assignment 2/producer.c
@@ -2,45 +2,148 @@
 #include <stdlib.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 #define NUM_BUFFER 100
-// typedef struct sembuf{
-//     short sem_num;
-//     short sem_op; //-1 for wait, +1 for signal
-//     short sem_flag;
-// } sembuf;
+
+//Structure to hold 128 bytes from a text file and byte count
+typedef struct buffer{
+    char data[128];
+    int count;
+} buffer;
 
 
 int main(){
 
     //Semaphore S
-    int success = semget((key_t) 1111, 1, 0666);
+    int S = semget((key_t) 1111, 1, 0777 | IPC_CREAT);
+
+    
+    if(S == -1){
+        perror("Could not create semaphore S\n");
+    }
+
+    int success = semctl(S, 0, SETVAL, 1);
+
+    if(success == -1){
+        perror("Could not set S semaphore value\n");
+    }
 
     //Semaphore E
-    if(success == -1){
-        perror("Could not create semaphore\n");
-    }
-    success = semget((key_t) 2222, 1, 0666);
-
-    //Semaphore N
-    if(success == -1){
-        perror("Could not create semaphore\n");
-    }
-
-    success = semget((key_t) 3333, 1, 0666);
-
-    if(success == -1){
-        perror("Could not create semaphore\n");
-    }
-
-    struct sembuf wait = {0, -1, SEM_UNDO};
+    int E = semget((key_t) 2222, 1, 0777 | IPC_CREAT);
     
-    FILE *fp = fopen("test.txt", "r");
+    if(E == -1){
+        perror("Could not create semaphore E\n");
+    }
 
-    success = shmget((key_t) 1234, (size_t) (128 + sizeof(int) * NUM_BUFFER), 0777 | IPC_CREAT);
+    success = semctl(E, 0, SETVAL, 100);
 
     if(success == -1){
+        perror("Could not set semaphore E value\n");
+    }
+    //Semaphore N
+    int N = semget((key_t) 3333, 1, 0777 | IPC_CREAT);
+
+    if(N == -1){
+        perror("Could not create semaphore N\n");
+    }
+    success = semctl(N, 0, SETVAL, 0);
+    if(success == -1){
+        perror("Could not set semaphore N value\n");
+    }
+    //structs for semop()
+    struct sembuf wait = {0, -1, SEM_UNDO};
+    struct sembuf signal = {0, 1, SEM_UNDO};
+    
+    
+    FILE *fp = fopen("read.txt", "r");
+
+    int shm_id = shmget((key_t) 1234, (size_t) (sizeof(buffer) * NUM_BUFFER), 0777 | IPC_CREAT);
+    
+    if(shm_id == -1){
         perror("could not create shared memory\n");
     }
+    buffer *shm;
+    shm = (buffer *)shmat(shm_id, (void *)0, 0);
+    if(shm == (buffer *)-1){
+        perror("could not attach shared memory\n");
+    }
+    
+    char *buf = malloc(BUFSIZ);
+    int in = 0;
+    buffer temp;
+    long int byte_count = 0;
 
+    
+    while(fgets(buf, BUFSIZ, fp)){
+        
+        //wait(e), wait(s)
+        
+        success = semop(E, &wait, 1);
+        if(success == -1){
+            perror("Semaphore E operation wait failed\n");
+        }
+        
+        success = semop(S, &wait, 1);
+        if(success == -1){
+            perror("Semaphore S operation wait failed\n");
+        }
+        //critical section
+        //if(sizeof(buf) > 128){
+            //break down buf into 128 Byte chunks for buffer structure
+            
+            for(int i = 0; i < BUFSIZ / 128; i++){
+                //Copy data into buffer structure
+                strncpy(temp.data, buf + (128 * i), 128);
+                // if(sizeof(buf + (128 * i)) < 128){
+                //     temp.data[sizeof(buf + (128 * i))] = '\0';
+                // }
+                
+                temp.count = sizeof(temp);
+                if(temp.data[0] == '\0'){
+                    continue;
+                }
+                //append(v)
+                shm[in] = temp;
+                printf("Producer copied string over to make: %s\n", shm[in].data);
+                in = (in + 1) % 100;
+                byte_count += sizeof(temp);
+                //signal(s), signal(n)
+                semop(S, &signal, 1);
+                semop(N, &signal, 1);
+            }
+        // } else {
+        //         //If buf is at the end of the file, it may be less than 128 bytes
+        //         printf("In the else statement\n");
+        //         strncpy(temp.data, buf, 128);
+        //         printf("Copied 128 bytes to make string: %s\n",temp.data);
+        //         temp.count = sizeof(buf);
+        //         shm[in] = temp;
+               
+        //         in = (in + 1) % 100;
+        //         byte_count += sizeof(temp);
+        //         success = semop(S, &signal, 1);
+        //         if(success == -1){
+        //             perror("S signal Failed\n");
+        //         }
+        //         success = semop(N, &signal, 1);
+        //         if(success == -1){
+        //             perror("N signal Failed\n");
+        //         }
+        // }
+
+
+    }
+    
+    printf("Total bytes written to shared memory: %ld\n", byte_count);
+    success = shmdt(shm);
+    if(success == -1){
+        perror("Could not detach shared memory\n");
+    }
+    fclose(fp);
+    if(success == -1){
+        perror("Could not close read file\n");
+    }
 
 }
